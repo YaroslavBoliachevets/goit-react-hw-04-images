e038d9f6f8cb3d4d5f7aa95986d6dd85cdfc34aa
YaroslavBoliachevets <boliachevets.ya@gmail.com>
2022-09-03 11:18:10 +0300

    рисует ответ в галерею

--- src/components/ImageGallery/ImageGallery.js
+++ src/components/ImageGallery/ImageGallery.js
@@ -1,32 +1,37 @@
 import React, { Component } from 'react';
-import axios from 'axios';
-import { ImageGallery } from './ImageGallery.module';
+import { ImageGallery, ImageGalleryItem, ImageGalleryItemImage } from './ImageGallery.module';
 
 class Gallery extends Component {
 
   state = {
     request: null,
+    images: null,
   }
 
   async componentDidUpdate(prevProps, prevState) {
     const axios = require('axios');
     const request = this.props.request;
-      try {
-        fetch(`https://pixabay.com/api/?key=29629491-d8b1867e90b1ff8305b24c06e&q=${request}`).then(r=>console.log(r))
+    const url = `https://pixabay.com/api/?q=${request}&page=1&key=29629491-d8b1867e90b1ff8305b24c06e&image_type=photo&orientation=horizontal&per_page=12`;
+    
 
+    if (prevProps.request !== request) {
+      try {
+        console.log('запрос', );
+        
+        axios.get(url).then(r=> {console.log(r.data.hits); this.setState({images: r.data.hits})})
       } catch (error) {
         console.error(error);
       }
-
-
-      console.log('gallery request', this.props.request)
+    }
   }
 
   render() {
     return (
       <>
         <ImageGallery>
-          <li>Images {this.props.request}</li>
+          {this.state.images && 
+          this.state.images.map(i => <ImageGalleryItem key={i.id}><ImageGalleryItemImage src={i.webformatURL} alt=""/></ImageGalleryItem> )}
+
         </ImageGallery>
       </>
     );
--- src/components/ImageGallery/ImageGallery.module.jsx
+++ src/components/ImageGallery/ImageGallery.module.jsx
@@ -12,3 +12,16 @@ export const ImageGallery = styled.ul`
   margin-left: auto;
   margin-right: auto;
 `;
+
+export const ImageGalleryItem = styled.li`
+  border-radius: 2px;
+  box-shadow: 0px 1px 3px 0px rgba(0, 0, 0, 0.2),
+    0px 1px 1px 0px rgba(0, 0, 0, 0.14), 0px 2px 1px -1px rgba(0, 0, 0, 0.12);
+`;
+
+export const ImageGalleryItemImage = styled.img`
+width: 100%;
+  height: 260px;
+  object-fit: cover;
+  transition: transform 250ms cubic-bezier(0.4, 0, 0.2, 1);
+`;
